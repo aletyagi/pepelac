5b3a2351df1d1359028ad95488dc5556b10f26ae
aletyagi <aletyagi@yandex.ru>
2020-12-13 17:30:36 +0300

    reptrain_first

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+use Intervention\Image\Facades\Image as ImageInt;
+use Illuminate\Support\Str;             //для Str::random()
+use Illuminate\Support\Facades\DB;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $images = Image::all();
+        return view('images.index',['images' => $images]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('images.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         $path = storage_path().'/app/public/images/';
+        $file = $request->file('file');
+
+        foreach ($file as $f) {
+        $filename = Str::random(10).'.'.$f->getClientOriginalExtension()?:'png';
+        $img = ImageInt::make($f);
+        $img->resize(200,200)->save($path.$filename);
+        Image::create(['title' => $request->title, 'img' => $filename]);
+    }
+
+    return redirect()->route('images.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/main.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    
+
+    <title>train/main</title>
+
+    
+</head>
+<body>
+<div>
+
+</div>
+    <div class="box-bottom">
+          <h2> Page0 </h2>
+          <a href="{{url('/resume')}}">ВИЗИТКА</a>
+    </div>
+    <p> bd_PostgreSQL   avel</p> 
+    </br>
+    <a href="{{url('/user/test')}}" > Enter </a> <a href="{{url('/reg')}}"> | Registration </a>
+<?php /*dump(app('router'));*/ ?>
+<?php /*dump(\Route::current());*/ /*это текущий маршрут*/?>
+</body>
+</html>
